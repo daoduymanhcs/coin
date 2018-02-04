0af1129f128fb8cabdb681180f085c7583b2d6ec
vmar <daoduymanhcs@gmail.com>
2018-02-04 23:41:44 +0700

    update repotr

--- app/Report.php
+++ app/Report.php
@@ -7,17 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     //
-    public function scopeReporthigh($query, $number)
+    public function scopeReporthigh($query, $number, $id)
     {
-        return $query->limit($number, $id)
+        return $query->limit($number)
         				->join('prices', 'reports.high_id', '=', 'prices.id')
         				->Where('reports', '=', $id)
         				->orderBy('reports.created_at', 'desc')
                	      	->get();
     }
-    public function scopeReportlow($query, $number)
+    public function scopeReportlow($query, $number, $id)
     {
-        return $query->limit($number, $id)
+        return $query->limit($number)
         				->join('prices', 'reports.low_id', '=', 'prices.id')
         				->Where('reports', '=', $id)
         				->orderBy('reports.created_at', 'desc')
